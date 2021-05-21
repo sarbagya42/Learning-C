7fb50883fbbbef1031b88cffe7265fdb2d9121e5
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-05-21 14:01:26 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+int main()
+{
+    int num1,store,c=0;
+    char len[10];
+    int num2,b,k;
+    printf("Type the number");
+    scanf("%d",&num1);
+    itoa(num1,len,10);
+    b=strlen(len);
+    k=b-1;
+    while(num1!=0){
+        num2=num1%10;
+        store=num2*pow(10,k)+store;
+        k=k-1;
+        num1=num1/10;
+        }
+        printf("%d",store);
+
+   return 0;
+}
