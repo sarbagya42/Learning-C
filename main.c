f910a4f67c19e8f6e9516ae79b4b5804cd0ce3a1
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-05-30 17:06:03 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+float days, year,month, remday;
+printf("Total days:");
+scanf("%f",&days);
+year=days/365;
+month=(year-(int)year)*12;
+remday=days-((int)year*365+(int)month*30);
+printf("\n%d Year%s %d Month%s %d Day%s\n",(int)year, (int)year!=1 ? "s":"", (int)month, (int)month!=1 ? "s":"", (int)remday, (int)remday!=1 ? "s":"");
+return 0;
+}
