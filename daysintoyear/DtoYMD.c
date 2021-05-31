b7aef23b59d27fd335b9c91a1f7cb75c8773f7bb
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-05-31 09:00:25 +0545

    Update DtoYMD.c

--- daysintoyear/DtoYMD.c
+++ daysintoyear/DtoYMD.c
@@ -3,12 +3,17 @@
 //Days into Year, Month and Days
 int main()
 {
-double days, year,month, remday;
+double days, year,month, remday,k;
 printf("Total days:");
 scanf("%lf",&days);
-year=days/364;
+year=days/365;
 month=(year-(int)year)*12;
-remday=(month-(int)month)*32;
-printf("\n%d Year%s %d Month%s %d Day%s\n",(int)year, (int)year!=1 ? "s":"", (int)month, (int)month!=1 ? "s":"", (int)remday, (int)remday!=1 ? "s":"");
+k=month-(int)month;
+printf("%f",k);
+remday=(k)*31;
+if(k>=0.5){
+    remday=remday+1;
+    }
+printf("\n%d Year%s %d Month%s %d Day%s\n",(int)year, (int)year!=1 ? "s":"", (int)month, (int)month!=1 ? "s":"",(int)remday, (int)remday!=1 ? "s":"");
 return 0;
 }
