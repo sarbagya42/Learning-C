0fad844bb3e4be4d3e072b28d92dd480db931fdb
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-02 13:24:15 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int num1,num2,num3,average;
+printf("Type three numbers:");
+scanf("%d" "%d" "%d",&num1,&num2,&num3);
+average=(num1+num2+num3)/3;
+printf("The Average is %d",average);
+return 0;
+}
+
