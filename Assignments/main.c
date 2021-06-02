b5680ef5b205e2b9ecf21fe356d23b3b8f2455a1
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-02 13:28:22 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+//SIMPLE INTEREST
+int main()
+{
+float P,T,R,SI;
+printf("Type Principle:");
+scanf("%f",&P);
+printf("Type Time:");
+scanf("%f",&T);
+printf("Type Rate of Interest:");
+scanf("%f",&R);
+SI=(P*T*R)/100;
+printf("The Average is %.3f",SI);
+return 0;
+}
+
