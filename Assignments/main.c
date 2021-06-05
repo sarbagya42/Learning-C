18ea2117824f3d86adfd13d5bf547115ad5d7c16
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-05 12:33:10 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Multiple String reading and printing
+
+int main()
+{
+ char a[10], b[10],c[10];
+ printf("Type 3 strings:");
+ scanf("%s %s %s", a,b,c);
+ printf("The strings are %s, %s, %s ", a,b,c);
+    return 0;
+}
