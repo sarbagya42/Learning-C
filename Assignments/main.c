702bccbe615d83729b4a6c91b8a0d2a11ec72447
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-05 11:53:40 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// ASCII value of any character
+
+int main()
+{
+    char a;
+    printf("Type any character:");
+    scanf("%c",&a);
+    printf("%d",a);
+    return 0;
+}
