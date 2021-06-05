77e52c3168a8d9b311d45afd982093afc3a93102
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-05 11:45:50 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// Interchanging value of two variables
+
+int main()
+{
+    int a=5,b=6,x;
+    x=a;
+    a=b;
+    b=x;
+    printf(" a=%d and b=%d",a,b);
+    return 0;
+}
