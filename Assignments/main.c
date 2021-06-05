b6a390f96323c8b1fa97af7e3fc76dcd832dcca1
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-05 11:35:14 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2, sum;
+    printf("Type two numbers:");
+    scanf("%d" "%d",&num1, &num2);
+    sum=num1+num2;
+    printf("The sum is: %d",sum);
+    return 0;
+}
