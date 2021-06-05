617a7487ae6dab0429675842923a1a8fd9b0e8e9
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-05 11:59:23 +0545

    Add files via upload

---
+++ Assignments/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+// Output Integer=9, Character=57
+
+int main()
+{
+    int n=4;
+    char c='4';
+    n=n+5;
+    c=c+5;
+    printf("Integer=%d",n);
+    printf("\nCharacter=%d",c);
+    return 0;
+}
