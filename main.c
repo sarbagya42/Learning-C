1f703a34ea74218f80dd8fd7975c7f396b7e7ea2
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-15 12:17:03 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c=0,i;
+    printf("Type any number:");
+    scanf("%d",&a);
+    for(i=1; i<=a; i++){
+        b=a%i;
+        if(b==0){
+            c=c+1;
+        }
+    }
+    if(c==2){
+        printf("\t\n%d is a Prime Number\n",a);
+    }else{
+        printf("\t\n%d is NOT a Prime Number\n",a);
+
+    }
+    return 0;
+}
