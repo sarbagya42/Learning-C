dfd66b0f2760e21ff32169c05db24c22f7667a48
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-15 13:29:22 +0545

    Add files via upload

--- Lab4/main.c
+++ Lab4/main.c
@@ -2,28 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-//Reverse and Palindrome
 int main()
 {
-    int num1,store,c=0,num2,b,k,s;
-    char len[10];
-    printf("Type the number:");
-    scanf("%d",&num1);
-    s=num1;
-    itoa(num1,len,10);
-    b=strlen(len);
-    k=b-1;
-    while(num1!=0){
-        num2=num1%10;
-        store=num2*pow(10,k)+store;
-        k=k-1;
-        num1=num1/10;
-        }
-        printf("\nThe reverse of the number %d is %d\n",s,store);
-        if(s==store){
-            printf("\nHence, it's Palindrome\n");
-        }else{
-        printf("\nHence, it's Not Palindrome\n");
-        }
-   return 0;
+  int n1,n2,i,b,c=0,m;
+  printf("Type the range:");
+  scanf("%d %d",&n1,&n2);
+  printf("The Armstrong Numbers in the range %d and %d are:",n1,n2);
+  for(i=n1; i<=n2; i++){
+        m=i;
+    while(m!=0){
+        b=m%10;
+        c=c+pow(b,3);
+        m=m/10;
+  }
+if(c==i){
+    printf("\n%d\n",i);
+  }
+c=0;
+  }
+return 0;
 }
