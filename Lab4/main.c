8ae957aa8528f2d5146f5d6b100efaa1df0f440a
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-15 12:52:59 +0545

    Add files via upload

---
+++ Lab4/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int x;
+    int y;
+    char fin;
+    char store[10];
+    char data[10];
+    printf("Type any decimal number:");
+    scanf_s("%d",&x);
+    while(x!=0){
+            y=x%2;
+            itoa(y,store,10);
+            strcat(data,store);
+            x=x/2;
+            }
+            printf("The Binary Equivalent of the provided number is: %s",strrev(data));
+}
