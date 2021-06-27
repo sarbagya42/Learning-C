7a64cae3eeb80e90f6f6765b4ea44f9f836a4135
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-06-27 18:37:01 +0545

    Add files via upload

---
+++ Lab 5(Function)/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int swap(int,int);
+int main()
+{
+    int a,b,store;
+    printf("Type any two numbers:");
+    scanf("%d %d",&a,&b);
+    printf("\nValue before swap is %d %d\n",a,b);
+    store=swap(a,b);
+    printf("\nValue after swap is %d %d\n",a,b);
+    return 0;
+}
+
+int swap(int x,int y)
+{
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+    printf("\nAfter swap is %d %d\n",x,y);
+}
