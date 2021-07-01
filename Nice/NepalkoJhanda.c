b61f8132cb059ff287f736ea0f5ea432d576176a
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-07-01 19:41:39 +0545

    Update NepalkoJhanda.c

--- Nice/NepalkoJhanda.c
+++ Nice/NepalkoJhanda.c
@@ -7,17 +7,18 @@ int main()
     int i;
     int j;
     int l;
+    int m=6;
+    int c=1;
     for (k = 1; k <= 2; k++) {
-        for (i = 1; i <= 6; i++) {
+        for (i = 2; i <= m+2*k; i++) {
             for (j = 1; j != i; j++) {
                 printf("69");
             }
             printf("\n");
         }
     }
-    for (l = 1; l <= 5; l++) {
+    for (l = 1; l <= 7; l++) {
         printf("69\n");
     }
-
     return 0;
 }
