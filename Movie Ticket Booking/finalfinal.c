25b2d2dc009026cc89a95b78811b0b8422c0685b
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-08-19 21:51:20 +0545

    Update finalfinal.c

--- Movie Ticket Booking/finalfinal.c
+++ Movie Ticket Booking/finalfinal.c
@@ -153,7 +153,7 @@ printf("\n\n\n\n\n\t\t\t\t\t\t\t\t\tType the Admin Password:");
     if(strcmp(ok,ma)==0)
     {
     printf("\n\n\t\t\t\t\t\t\t\t\tTYPE '1' to INSERT MOVIE\n\n\t\t\t\t\t\t\t\t\tTYPE '2' TO VIEW ALL TRANSACTIONS\n\n\t\t\t\t\t\t\t\t\tTYPE '3' TO VIEW ALL REGISTRATIONS");
-    printf("\n\n\n\t\t\t\t\t\t\t\tType:");
+    printf("\n\n\n\t\t\t\t\t\t\t\t\tTYPE:");
     scanf("%d",&p);
     if(p==1)
     {
