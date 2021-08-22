e347a6683dfa435ffbc44a5808843b771a05b2e0
Sarbagya <83492985+69sarbu69@users.noreply.github.com>
2021-08-22 09:16:03 +0545

    Update Fibonaccigenerator.c

--- Fibonacci/Fibonaccigenerator.c
+++ Fibonacci/Fibonaccigenerator.c
@@ -9,9 +9,21 @@ int main()
     int c,i,n;
     printf("Type number of Fibonacci Terms you want:");
     scanf("%d",&n);
+    if(n==1)
+    {
+    printf("%d\n",a);
+    }
+    if(n==2)
+    {
     printf("%d\n",a);
     printf("%d\n",b);
-    for(i=1; i!=n-1; i++){
+    }
+    else
+    {
+    printf("%d\n",a);
+    printf("%d\n",b);
+    }
+    for(i=1; i<=n-2; i++){
         c=a+b;
         printf("%d\n",c);
         a=b;
